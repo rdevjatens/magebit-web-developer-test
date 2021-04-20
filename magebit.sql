9dcaacfe07ec494fd0470be2230b9013df53ab11
Romalds Devjateņs <39839208+rdevjatens@users.noreply.github.com>
2021-04-20 14:15:34 +0300

    Update magebit.sql

--- magebit.sql
+++ magebit.sql
@@ -18,18 +18,19 @@ USE `magebit`;
 
 -- Dumping structure for table magebit.emails
 CREATE TABLE IF NOT EXISTS `emails` (
-  `email` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
   `host` varchar(45) DEFAULT NULL,
-  `date` date DEFAULT NULL
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table magebit.emails: ~4 rows (approximately)
 /*!40000 ALTER TABLE `emails` DISABLE KEYS */;
 INSERT INTO `emails` (`email`, `host`, `date`) VALUES
-	('test@gmail.com', 'gmail.com', '2021-04-20'),
-	('example@gmail.com', 'gmail.com', '2021-04-20'),
-	('sample@yahoo.com', 'yahoo.com', '2021-04-19'),
-	('newemail@test.com', 'test.com', '2021-04-15');
+	('example@yahho.com', 'yahho.com', '2020-04-19'),
+	('rdevjatens@gmail.com', 'gmail.com', '2021-04-18'),
+	('sample@gmail.com', 'gmail.com', '2021-04-20'),
+	('test@test.com', 'test.com', '2021-04-20');
 /*!40000 ALTER TABLE `emails` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
